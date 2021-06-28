1ef4258690760dcd533b1745a0f68fb5749bb40e
Mihail Marian <mihailmarian12@gmail.com>
2021-06-28 11:09:32 +0100

    init file

---
+++ greeter.js
@@ -0,0 +1,3 @@
+var greeter = function (person) { return "Hello, " + person; };
+var user = "Jane User";
+document.body.textContent = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,5 @@
+const greeter = (person) => "Hello, " + person;
+
+let user = "Jane User";
+
+document.body.textContent = greeter(user);
